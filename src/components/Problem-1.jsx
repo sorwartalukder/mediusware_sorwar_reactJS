77dd961abe3e4d83035de050fee33e68acc2fdea
sorwartalukder <mdsorwar4039@gmail.com>
2023-12-28 15:08:30 +0600

    done task

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,59 +1,141 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const Problem1 = () => {
+  const [allData, setAllData] = useState([]);
+  const [tableData, setTableData] = useState([]);
+  const [addTask, setAddTask] = useState({
+    name: "",
+    status: "",
+  });
 
-    const [show, setShow] = useState('all');
+  const handleChange = (e) => {
+    setAddTask({ ...addTask, [e.target.name]: e.target.value });
+  };
 
-    const handleClick = (val) =>{
-        setShow(val);
+  const hanldeTaskAdd = (e) => {
+    e.preventDefault();
+
+    setAllData([...allData, addTask]);
+
+    setTableData([...tableData, addTask]);
+
+    setAddTask({
+      name: "",
+      status: "",
+    });
+  };
+
+  const [show, setShow] = useState("all");
+  const handleClick = (val) => {
+    setShow(val);
+  };
+
+  useEffect(() => {
+    if (show == "all") {
+      setTableData(allData);
+    } else if (show == "active") {
+      const activeData = allData?.filter(
+        (data) => data?.status.toLowerCase() == "active"
+      );
+      setTableData(activeData);
+    } else if (show == "completed") {
+      const completedData = allData.filter(
+        (data) => data.status.toLowerCase() == "completed"
+      );
+      setTableData(completedData);
     }
+  }, [show]);
 
-    return (
-
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
-                <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
-                        </div>
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
-                        </div>
-                        <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="col-8">
-                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
-                        </li>
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
-                        </li>
-                    </ul>
-                    <div className="tab-content"></div>
-                    <table className="table table-striped ">
-                        <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        
-                        </tbody>
-                    </table>
-                </div>
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+        <div className="col-6 ">
+          <form
+            onSubmit={hanldeTaskAdd}
+            className="row gy-2 gx-3 align-items-center mb-4"
+          >
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+                value={addTask.name}
+                onChange={handleChange}
+              />
             </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Status"
+                name="status"
+                value={addTask.status}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-    );
+
+        <div className="col-8">
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "all" && "active"}`}
+                type="button"
+                onClick={() => handleClick("all")}
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "active" && "active"}`}
+                type="button"
+                onClick={() => handleClick("active")}
+              >
+                Active
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "completed" && "active"}`}
+                type="button"
+                onClick={() => handleClick("completed")}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
+          <div className="tab-content"></div>
+          <table className="table table-striped ">
+            <thead>
+              <tr>
+                {/* <th scope="col">#</th> */}
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData?.map((data, index) => (
+                <tr key={index}>
+                  <td>{data?.name}</td>
+                  <td>{data?.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Problem1;
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,249 @@
-import React from 'react';
+import React, { useState } from "react";
+import * as bootstrap from "bootstrap";
 
 const Problem2 = () => {
+  const [allContact, setAllContact] = useState([]);
+  const [usContact, setUsContact] = useState([]);
+  const [contactDetails, setContactDetails] = useState({});
 
-    return (
+  console.log(contactDetails);
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
-        </div>
+  // get all contact
+  const handleAllContact = async () => {
+    const response = await fetch(
+      "https://contact.mediusware.com/api/contacts/"
     );
+    const contacts = await response.json();
+    setAllContact(contacts?.results);
+  };
+
+  //get us contact
+  const handleUsContact = async () => {
+    const response = await fetch(
+      "https://contact.mediusware.com/api/country-contacts/United%20States/"
+    );
+    const contact = await response.json();
+    setUsContact(contact?.results);
+  };
+
+  //get all even contact
+  const handleEvenAllContact = (e) => {
+    if (e.target.type === "checkbox" ? e.target.checked : e.target.value) {
+      const evenContacts = allContact.filter(
+        (contact) => contact.id && !(contact.id % 2) && contact
+      );
+      setAllContact(evenContacts);
+    } else {
+      handleAllContact();
+    }
+  };
+
+  // get us even contact
+  const handleEvenUsContact = (e) => {
+    if (e.target.type === "checkbox" ? e.target.checked : e.target.value) {
+      const evenContacts = usContact.filter(
+        (contact) => contact.id && !(contact.id % 2) && contact
+      );
+      setUsContact(evenContacts);
+    } else {
+      handleUsContact();
+    }
+  };
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            className="btn btn-lg btn-outline-primary"
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#modalA"
+            onClick={handleAllContact}
+          >
+            All Contacts
+          </button>
+          <button
+            className="btn btn-lg btn-outline-warning"
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#modalB"
+            onClick={handleUsContact}
+          >
+            US Contacts
+          </button>
+        </div>
+      </div>
+
+      {/* all contact modal */}
+      <div
+        className="modal fade"
+        id="modalA"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                All Contact
+              </h1>
+            </div>
+            <div className="modal-body">
+              {allContact?.map((contact) => (
+                <p
+                  key={contact?.id}
+                  className="d-block my-2 border-0 text-primary"
+                  style={{ cursor: "pointer" }}
+                  data-bs-toggle="modal"
+                  data-bs-target="#modalC"
+                  onClick={() => setContactDetails(contact)}
+                >
+                  {contact?.phone}
+                </p>
+              ))}
+            </div>
+            <div className="modal-footer justify-content-between w-100">
+              <input
+                onClick={handleEvenAllContact}
+                type="checkbox"
+                name="even"
+                id=""
+              />
+              <div>
+                <button
+                  type="button"
+                  className="btn mx-1"
+                  data-bs-dismiss="modal"
+                  style={{ background: "white", border: "1px solid #46139f" }}
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  className="btn mx-1 border-0 text-white"
+                  data-bs-toggle="modal"
+                  data-bs-target="#modalB"
+                  style={{ background: "#ff7f50" }}
+                >
+                  US contacts
+                </button>
+                <button
+                  type="button"
+                  className="btn mx-1 border-0 text-white"
+                  style={{ background: "#46139f" }}
+                >
+                  All Contacts
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* us contact modal */}
+      <div
+        className="modal fade"
+        id="modalB"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                US contact
+              </h1>
+            </div>
+            <div className="modal-body">
+              {usContact?.map((contact) => (
+                <p
+                  key={contact?.id}
+                  className="text-primary"
+                  style={{ cursor: "pointer" }}
+                  data-bs-toggle="modal"
+                  data-bs-target="#modalC"
+                  onClick={() => setContactDetails(contact)}
+                >
+                  {contact?.phone}
+                </p>
+              ))}
+            </div>
+            <div className="modal-footer justify-content-between w-100">
+              <input
+                onClick={handleEvenUsContact}
+                type="checkbox"
+                name="even"
+                id=""
+              />
+              <div>
+                <button
+                  type="button"
+                  className="btn mx-1"
+                  data-bs-dismiss="modal"
+                  style={{ background: "white", border: "1px solid #46139f" }}
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  className="btn mx-1 border-0 text-white"
+                  style={{ background: "#ff7f50" }}
+                >
+                  US contacts
+                </button>
+                <button
+                  type="button"
+                  className="btn mx-1 border-0 text-white"
+                  data-bs-toggle="modal"
+                  data-bs-target="#modalA"
+                  style={{ background: "#46139f" }}
+                >
+                  All Contacts
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* contact details modal */}
+      <div
+        className="modal fade"
+        id="modalC"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Contact Details
+              </h1>
+            </div>
+            <div className="modal-body">
+              <h4>Contry: {contactDetails?.country?.name}</h4>
+              <p>Phone : {contactDetails?.phone}</p>
+            </div>
+            <div className="modal-footer ">
+              <button
+                type="button"
+                className="btn"
+                data-bs-dismiss="modal"
+                style={{ background: "white", border: "1px solid #46139f" }}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
